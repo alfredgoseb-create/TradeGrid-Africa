deacbd42baa91d6cc7d5b5d5a11630f095d55aa2
alfredgoseb-create <alfredgoseb@gmail.com>
2026-03-25 10:41:03 +0000

    Remove duplicate JS pages and clean Next.js build

--- pages/shipments.tsx
+++ pages/shipments.tsx
@@ -1,1 +1,12 @@
-  
+  import React from 'react';
+
+const ShipmentsPage = () => {
+  return (
+    <div>
+      <h1>Shipments</h1>
+      {/* Your page content */}
+    </div>
+  );
+};
+
+export default ShipmentsPage;
